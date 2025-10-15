73c32f1ce48a26c25dfdc4af9f4c6de90b3265c7
adityaraj9798 <adityaraj97989@gmail.com>
2025-10-16 00:49:17 +0530

    calculator
    this calculator show the faulty value. this is 90 percent precised

---
+++ index.js
@@ -0,0 +1,21 @@
+let random= Math.random()
+console.log(random)
+let a=prompt("Enter the first number")
+let b=prompt("Enter operation")
+let c=prompt("Enter second number")
+
+let obj= {
+    "+": "-",
+    "*": "+",
+    "-": "/",
+    "/": "**",
+}
+if(random > 0.1){
+    console.log(`the result is ${a} ${c} ${b}`)
+    alert(`the result is ${eval(`${a} ${c} ${b}`)}`)
+}
+
+else{
+    c=obj[c]
+    alert(`the result is ${eval(`${a} ${c} ${b}`)}`)
+}
