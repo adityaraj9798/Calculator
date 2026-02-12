0eb0c2a10058537998179609b3da1b44c8dc7146
adityaraj9798 <adityaraj97989@gmail.com>
2026-02-12 14:25:58 +0530

    Add a Semicolon

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let random= Math.random()
 console.log(random)
 let a=prompt("Enter the first number")
 let b=prompt("Enter operation")
-let c=prompt("Enter second number")
+let c=prompt("Enter second number");
 
 let obj= {
     "+": "-",
